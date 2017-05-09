c664c7b7182bf43de7d940e04cb27617e349daf2
VuThuy22 <vthuy2222@gmail.com>
2017-05-10 01:15:29 +0700

    kết quả học tập

---
+++ Phan mem quan li GV-HS/WindowsFormsApplication1/KQHT.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class KQHT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HanhKiem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbLop = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbNS = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbMaHS = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbHT = new System.Windows.Forms.Label();
+            this.TBCN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TBHK2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TBHK1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvTTHT = new System.Windows.Forms.DataGridView();
+            this.NamHoc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HocLuc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTTHT)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // HanhKiem
+            // 
+            this.HanhKiem.DataPropertyName = "HanhKiem";
+            this.HanhKiem.HeaderText = "Hạnh Kiểm";
+            this.HanhKiem.Name = "HanhKiem";
+            this.HanhKiem.ReadOnly = true;
+            this.HanhKiem.Width = 90;
+            // 
+            // lbLop
+            // 
+            this.lbLop.AutoSize = true;
+            this.lbLop.Location = new System.Drawing.Point(568, 103);
+            this.lbLop.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbLop.Name = "lbLop";
+            this.lbLop.Size = new System.Drawing.Size(0, 20);
+            this.lbLop.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(466, 103);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Lớp:";
+            // 
+            // lbNS
+            // 
+            this.lbNS.AutoSize = true;
+            this.lbNS.Location = new System.Drawing.Point(568, 45);
+            this.lbNS.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbNS.Name = "lbNS";
+            this.lbNS.Size = new System.Drawing.Size(0, 20);
+            this.lbNS.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(466, 45);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ngày Sinh:";
+            // 
+            // lbMaHS
+            // 
+            this.lbMaHS.AutoSize = true;
+            this.lbMaHS.Location = new System.Drawing.Point(156, 103);
+            this.lbMaHS.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbMaHS.Name = "lbMaHS";
+            this.lbMaHS.Size = new System.Drawing.Size(0, 20);
+            this.lbMaHS.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 103);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mã HS: ";
+            // 
+            // lbHT
+            // 
+            this.lbHT.AutoSize = true;
+            this.lbHT.Location = new System.Drawing.Point(156, 45);
+            this.lbHT.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbHT.Name = "lbHT";
+            this.lbHT.Size = new System.Drawing.Size(0, 20);
+            this.lbHT.TabIndex = 1;
+            // 
+            // TBCN
+            // 
+            this.TBCN.DataPropertyName = "TBCN";
+            this.TBCN.HeaderText = "TBCN";
+            this.TBCN.Name = "TBCN";
+            this.TBCN.ReadOnly = true;
+            this.TBCN.Width = 90;
+            // 
+            // TBHK2
+            // 
+            this.TBHK2.DataPropertyName = "TBHK2";
+            this.TBHK2.HeaderText = "TBHK2";
+            this.TBHK2.Name = "TBHK2";
+            this.TBHK2.ReadOnly = true;
+            this.TBHK2.Width = 90;
+            // 
+            // TBHK1
+            // 
+            this.TBHK1.DataPropertyName = "TBHK1";
+            this.TBHK1.HeaderText = "TBHK1";
+            this.TBHK1.Name = "TBHK1";
+            this.TBHK1.ReadOnly = true;
+            this.TBHK1.Width = 90;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Họ Tên:";
+            // 
+            // dgvTTHT
+            // 
+            this.dgvTTHT.AllowUserToAddRows = false;
+            this.dgvTTHT.AllowUserToDeleteRows = false;
+            this.dgvTTHT.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTTHT.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NamHoc,
+            this.TBHK1,
+            this.TBHK2,
+            this.TBCN,
+            this.HocLuc,
+            this.HanhKiem});
+            this.dgvTTHT.Location = new System.Drawing.Point(9, 35);
+            this.dgvTTHT.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvTTHT.Name = "dgvTTHT";
+            this.dgvTTHT.ReadOnly = true;
+            this.dgvTTHT.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvTTHT.Size = new System.Drawing.Size(951, 355);
+            this.dgvTTHT.TabIndex = 0;
+            // 
+            // NamHoc
+            // 
+            this.NamHoc.DataPropertyName = "NamHoc";
+            this.NamHoc.HeaderText = "Năm Học";
+            this.NamHoc.Name = "NamHoc";
+            this.NamHoc.ReadOnly = true;
+            // 
+            // HocLuc
+            // 
+            this.HocLuc.DataPropertyName = "HocLuc";
+            this.HocLuc.HeaderText = "Học Lực";
+            this.HocLuc.Name = "HocLuc";
+            this.HocLuc.ReadOnly = true;
+            this.HocLuc.Width = 90;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvTTHT);
+            this.groupBox2.Location = new System.Drawing.Point(36, 263);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(969, 400);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thông tin học tập";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbLop);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.lbNS);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lbMaHS);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lbHT);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(36, 39);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(969, 166);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin cá nhân";
+            // 
+            // KQHT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1041, 702);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "KQHT";
+            this.Text = "KQHT";
+            this.Load += new System.EventHandler(this.KQHT_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTTHT)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn HanhKiem;
+        private System.Windows.Forms.Label lbLop;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbNS;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbMaHS;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbHT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TBCN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TBHK2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TBHK1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvTTHT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NamHoc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HocLuc;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ Phan mem quan li GV-HS/WindowsFormsApplication1/KQHT.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class KQHT : Form
+    {
+        public static string mahs = string.Empty, tenhs = string.Empty, ns = string.Empty, lop = string.Empty;
+        public KQHT()
+        {
+            InitializeComponent();
+        }
+
+        private void KQHT_Load(object sender, EventArgs e)
+        {
+            lbHT.Text = tenhs;
+            lbMaHS.Text = mahs;
+            lbNS.Text = ns;
+            lbLop.Text = lop;
+
+            string sql = "XemKQHT ";
+            SqlCommand com = new SqlCommand(sql, Login.con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@mahs", mahs);
+
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgvTTHT.DataSource = dt;
+            dgvTTHT.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+    }
+}

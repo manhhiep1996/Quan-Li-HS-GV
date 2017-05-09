d6354f1393947e074fa73dd3e83f321d526babef
TranDung8396 <trandung8396@gmail.com>
2017-05-10 02:03:27 +0700

    Commit Login & Huong Dan

--- Phan mem quan li GV-HS/WindowsFormsApplication1/HuongDan.Designer.cs
+++ Phan mem quan li GV-HS/WindowsFormsApplication1/HuongDan.Designer.cs
@@ -28,19 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HuongDan));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(500, 365);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
             // 
             // HuongDan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(500, 365);
+            this.Controls.Add(this.richTextBox1);
             this.Name = "HuongDan";
             this.Text = "HuongDan";
+            this.Load += new System.EventHandler(this.HuongDan_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
--- Phan mem quan li GV-HS/WindowsFormsApplication1/Login.Designer.cs
+++ Phan mem quan li GV-HS/WindowsFormsApplication1/Login.Designer.cs
@@ -92,6 +92,7 @@
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(164, 20);
             this.txtName.TabIndex = 10;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
             // 
             // label2
             // 

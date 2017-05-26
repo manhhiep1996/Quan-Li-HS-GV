04c739451284bf43e4185b1a232fe2a0164df459
TranDung8396 <trandung8396@gmail.com>
2017-05-26 16:43:31 +0700

    Login+HuongDan_Dung

--- Phan mem quan li GV-HS/WindowsFormsApplication1/HuongDan.cs
+++ Phan mem quan li GV-HS/WindowsFormsApplication1/HuongDan.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void HuongDan_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- Phan mem quan li GV-HS/WindowsFormsApplication1/Login.Designer.cs
+++ Phan mem quan li GV-HS/WindowsFormsApplication1/Login.Designer.cs
@@ -28,21 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbWrong = new System.Windows.Forms.Label();
+            this.ckbShow = new System.Windows.Forms.CheckBox();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.btLogin = new System.Windows.Forms.Button();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lbWrong
+            // 
+            this.lbWrong.AutoSize = true;
+            this.lbWrong.Location = new System.Drawing.Point(67, 147);
+            this.lbWrong.Name = "lbWrong";
+            this.lbWrong.Size = new System.Drawing.Size(0, 13);
+            this.lbWrong.TabIndex = 23;
+            // 
+            // ckbShow
+            // 
+            this.ckbShow.AutoSize = true;
+            this.ckbShow.Location = new System.Drawing.Point(96, 116);
+            this.ckbShow.Name = "ckbShow";
+            this.ckbShow.Size = new System.Drawing.Size(102, 17);
+            this.ckbShow.TabIndex = 22;
+            this.ckbShow.Text = "Show Password";
+            this.ckbShow.UseVisualStyleBackColor = true;
+            // 
+            // btCancel
+            // 
+            this.btCancel.Location = new System.Drawing.Point(164, 163);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(75, 23);
+            this.btCancel.TabIndex = 21;
+            this.btCancel.Text = "Cancel";
+            this.btCancel.UseVisualStyleBackColor = true;
+            this.btCancel.Click += new System.EventHandler(this.btCancel_Click);
+            // 
+            // btLogin
+            // 
+            this.btLogin.Location = new System.Drawing.Point(45, 163);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(75, 23);
+            this.btLogin.TabIndex = 20;
+            this.btLogin.Text = "Login";
+            this.btLogin.UseVisualStyleBackColor = true;
+            this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(96, 81);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(164, 20);
+            this.txtPass.TabIndex = 19;
+            this.txtPass.UseSystemPasswordChar = true;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(96, 43);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(164, 20);
+            this.txtName.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Username";
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 229);
+            this.ClientSize = new System.Drawing.Size(300, 242);
+            this.Controls.Add(this.lbWrong);
+            this.Controls.Add(this.ckbShow);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.btLogin);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Login";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbWrong;
+        private System.Windows.Forms.CheckBox ckbShow;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.Button btLogin;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
